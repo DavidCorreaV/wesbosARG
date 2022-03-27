ecf184ff697ab75c1b39e506664822c4eaae9b26
DavidCorreaV <david.correav@gmail.com>
2022-03-26 23:52:27 -0500

    Boilerplate for Apollo

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -3,7 +3,10 @@ import '../components/styles/nprogress.css';
 
 import Router from 'next/router';
 
+import { ApolloProvider } from '@apollo/client';
 import Page from '../components/Page';
+
+import withData from '../lib/withData';
 
 Router.events.on('routeChangeStart', () => {
   NProgress.start();
@@ -15,10 +18,20 @@ Router.events.on('routeChangeError', () => {
   NProgress.done();
 });
 // eslint-disable-next-line react/prop-types
-const App = ({ Component, pageProps }) => (
-  <Page>
-    {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-    <Component {...pageProps} />
-  </Page>
+const App = ({ Component, pageProps, apollo }) => (
+  <ApolloProvider client={apollo}>
+    <Page>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
+    </Page>
+  </ApolloProvider>
 );
-export default App;
+
+App.getInitialProps = async ({ Component, ctx }) => {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+};
+export default withData(App);
